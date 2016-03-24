4db43ed05c3d5611445493568271f09fdb5b2ff8
Maia Ross <rainbowroach@gmail.com>
2016-03-24 12:58:11 -0400

    slight efficiency change, still having problems.

--- src/shortestpath/Main.java
+++ src/shortestpath/Main.java
@@ -40,77 +40,84 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String[] args) {
-        //A: read input
-        Scanner sc = new Scanner(System.in);
-        try {
-            int shortestPath = 0;
-            //READ INPUT
-            Input input = new Input("C:\\Users\\Maia\\Desktop\\25000.txt");
-            String[] nodes = input.getNodes();
-            Edge[] edges = input.getEdges();
-
-            //CONVERT EDGES TO BIG BIHEAP
-            BiHeap heap = new BiHeap(edges.length);
-            for (int i = 0; i < edges.length; i++) {
-                heap.insert(edges[i]);
+        while (true) {
+            //A: read input
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Input file address or e to exit: ");// C:\\Users\\Maia\\Desktop\\
+            String fileName = sc.next().trim();
+            if (fileName.equals("e")) {
+                System.exit(0);
             }
-            //LIST OF VISITED NODES
-            LinkedList visitedNodes = new LinkedList();
-            
-            //TEST
-            int remainingEdges=edges.length;
 
-            //WHILE THERE ARE REMIANING NODES
-            while (!heap.isEmpty()) {
-                //loop prep work
-                Edge temp = heap.deleteMin();
-                Boolean visited = false;
-                String source = temp.a;
-                String dest = temp.b;
-                //done prep work
-                
+            try {
+                int shortestPath = 0;
+                //READ INPUT
+                Input input = new Input(fileName);
+                String[] nodes = input.getNodes();
+                Edge[] edges = input.getEdges();
+
+                //CONVERT EDGES TO BIG BIHEAP
+                BiHeap heap = new BiHeap(edges.length);
+                for (int i = 0; i < edges.length; i++) {
+                    heap.insert(edges[i]);
+                }
+                //LIST OF VISITED NODES
+                LinkedList visitedNodes = new LinkedList();
+
                 //TEST
-                //System.out.println("Remaining edges: " + remainingEdges--);
-                //System.out.println("Checking source: " + temp.a + " dest: " + temp.b + " weight: " + temp.weight);
-                
-                for (Object visitedNode : visitedNodes) {
-                    if (source.equals(visitedNode)) {
-                        visited=true;
-                        //TEST
-                        //System.out.println("Already visited.");
-                        break;
-                    }//if
-                } //for
-                
-                //if it''s a new source, then look through all of source's edges and find min
-                if (!visited){
-                    for (int i = 0; i < remainingEdges; i++){
-                        //System.out.println("Double checking: " + temp.a + " dest: " + temp.b + " weight: " + temp.weight);
-                        if (source.equals(heap.peek(i).a) && heap.peek(i).weight<temp.weight){
-                            //if they have the same source and alt's weight is smaller
-                            //make temp equal to alt
+                int remainingEdges = edges.length;
+
+                //WHILE THERE ARE REMIANING NODES
+                while (!heap.isEmpty()) {
+                    //loop prep work
+                    Edge temp = heap.deleteMin();
+                    Boolean visited = false;
+                    String source = temp.a;
+                    String dest = temp.b;
+                //done prep work
+
+                    //TEST
+                   // System.out.println("Remaining edges: " + remainingEdges--);
+                    //System.out.println("Checking source: " + temp.a + " dest: " + temp.b + " weight: " + temp.weight);
+                    for (Object visitedNode : visitedNodes) {
+                        if (source.equals(visitedNode)) {
+                            visited = true;
                             //TEST
-                            //System.out.println("Better source: " + temp.a + " dest: " + temp.b + " weight: " + temp.weight);
-                            temp=heap.peek(i);
+                            //System.out.println("Already visited.");
+                            break;
                         }//if
-                    }//for
-                    //add temp's weight to shortestPath
-                    shortestPath+=temp.weight;
-                    //System.out.println("Updated path: " + shortestPath);
-                }//if
-                
-                //add to visited node
-                visitedNodes.add(source);
-                
-            }//while
-            
-            System.out.println("Final shortest path: " + shortestPath);
+                    } //for
 
-        } catch (FileNotFoundException ex) {
-            System.out.print("File not found.");
-            System.exit(1);
-        }//catch
+                    //if it''s a new source, then look through all of source's edges and find min
+                    if (!visited) {
+                        /*
+                         for (int i = 0; i < remainingEdges; i++) {
+                         //System.out.println("Double checking: " + temp.a + " dest: " + temp.b + " weight: " + temp.weight);
+                         if (source.equals(heap.peek(i).a) && heap.peek(i).weight < temp.weight) {
+                         //if they have the same source and alt's weight is smaller
+                         //make temp equal to alt
+                         //TEST
+                         System.out.println("Better source: " + temp.a + " dest: " + temp.b + " weight: " + temp.weight);
+                         temp = heap.peek(i);
+                         }//if
+                         }//for
+                         */
+                        //add temp's weight to shortestPath
+                        shortestPath += temp.weight;
+                        //System.out.println("Updated path: " + shortestPath);
+                    }//if
 
+                    //add to visited node
+                    visitedNodes.add(source);
+
+                }//while
+
+                System.out.println("Final shortest path: " + shortestPath);
+
+            } catch (FileNotFoundException ex) {
+                System.out.println("File not found.");
+            }//catch
+        }
     }//main
 
 }//class
